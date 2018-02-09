ebe829853a316a055a5efc50808d8e5ed979a55c
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-02-09 06:37:59 +0000

    added script to download collections of yang models

---
+++ Models-External/Download.ps1
@@ -0,0 +1,41 @@
+[CmdletBinding(SupportsShouldProcess=$True)]
+param(
+    [switch]$Update
+)
+
+[PSCustomObject[]]$repositories = @(
+    [PSCustomObject]@{ Name = "BroadbandForum";   Link = "https://github.com/BroadbandForum/yang.git" },
+    [PSCustomObject]@{ Name = "Juniper";   Link = "https://github.com/Juniper/yang.git" },
+    [PSCustomObject]@{ Name = "networkop";   Link = "https://github.com/networkop/yang.git" },
+    [PSCustomObject]@{ Name = "sysrepo";   Link = "https://github.com/sysrepo/sysrepo.git" },
+    [PSCustomObject]@{ Name = "tail-f-systems";   Link = "https://github.com/tail-f-systems/JNC.git" },
+    [PSCustomObject]@{ Name = "YangModels";   Link = "https://github.com/YangModels/yang.git" }
+)
+
+Push-Location -Path $PSScriptRoot
+
+foreach($repo in $repositories) {
+    $name = $repo.Name
+
+    Write-Verbose -Message "Checking $name"
+
+    if (-not (Test-Path -Path $name -PathType Container)) {
+        Write-Verbose -Message "Cloning $name"
+
+        if($PSCmdlet.ShouldProcess("$name", "Cloning (git clone)")) {
+            git clone --recurse-submodule $repo.Link $name
+        }
+    } else {
+        Write-Verbose -Message "Repo $name is present"
+
+        if ($Update) {
+            if ($PSCmdlet.ShouldProcess("$name", "Updating (git pull)")) {
+                Push-Location -Path $name
+                git pull
+                Pop-Location
+            }
+        }
+    }
+}
+
+Pop-Location
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # YangDotNet
 Parser and .NET type generator for YANG model
+
+## Folder structure
+
+This repo contains the following directories:
+
+- `Models-External`: Contains repos to sources of YANG models. These are not linked
+  as submodules, since some of them are quite big, and they are not always needed
+  for development. To retrieve them run the `Download.ps1` PowerShell script from
+  that directory.
+
