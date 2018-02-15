89b5c6729a194e7a1b56db9cfb4320296ff5c652
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-02-15 10:07:36 +0000

    fix(Parser): Properly parsing sub-blocks (for the generic block parser)

--- src/YANG/Parser/Playground.fsx
+++ src/YANG/Parser/Playground.fsx
@@ -40,36 +40,63 @@ let (<!>) (p: Parser<_,_>) label : Parser<_,_> =
         printfn "%A: Leaving %s (%A)" stream.Position label reply.Status
         reply
 
+/// Tracks the state of a generic parser
 type TextState =
+/// The parser is in normal state, i.e. not in a string
 | Normal
+/// The parser is in a single-quoted string
 | SingleQuotedString
+/// The parser is in a double-quoted string
 | DoubleQuotedString
+/// The parser is in a double-quoted string and the previous character was an escape,
+/// hence a double quote is not the end of the string.
 | Escaped
 
+/// <summary>
+/// Parses a block as a string. The block starts with '{',
+/// ends with '}', can contain a number of nested sub-blocks
+/// (which should be paired), and can contain arbitrary characters
+/// inside single- and double- quoted strings.
+/// </summary>
 let unparsed_block_literal<'a> : Parser<string, 'a> =
     let mutable opened = 0
     let mutable state : TextState = Normal
 
+    /// Updates the state of the parser based on the current character
     let closing c : bool =
         match state, c with
+        // Exiting single quoted string
         | SingleQuotedString, '\''  -> state <- Normal; true
+        // Keep parsing single quoted string
         | SingleQuotedString, _     -> true
+        // Exiting double quoted string
         | DoubleQuotedString, '"'   -> state <- Normal; true
+        // Entering escape character
         | DoubleQuotedString, '\\'  -> state <- Escaped; true
+        // Keep parsing double quoted string
         | DoubleQuotedString, _     -> true
+        // Exit escape character
         | Escaped, _                -> state <- DoubleQuotedString; true
+        // Enter single quoted string
         | Normal, '\''              -> state <- SingleQuotedString; true
+        // Enter double quoted string
         | Normal, '"'               -> state <- DoubleQuotedString; true
+        // Beginning of a new block (sub-block)
+        | Normal, '{'               -> opened <- opened + 1; true
+        // End of a block
         | Normal, '}'               ->
             if opened = 0 then
+                // End of the main block
                 false
             else
+                // End of sub-block
                 opened <- opened - 1
                 true
+        // Continue parsing
         | Normal, _else             -> true
 
     let normalChar = satisfy closing
-    manyChars normalChar
+    skipChar '{' >>. manyChars normalChar .>> skipChar '}'
 
 
 type Statement =
@@ -84,10 +111,8 @@ type Module =
 let module_parser<'a> : Parser<Module, 'a> =
     let parser =
         spaces >>. skipStringCI "module" >>. spaces >>.
-        Identifier.parse_identifier .>> spaces .>>
-        skipChar '{' .>> spaces .>>.
-        unparsed_block_literal .>> spaces .>>
-        skipChar '}' .>> spaces
+        Identifier.parse_identifier .>> spaces .>>.
+        unparsed_block_literal
     parser |>> (
         fun (identifier, block) ->
             {
