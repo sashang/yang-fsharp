f9bb1e4e49c158f1f9eb177e9638efcfdc75f9da
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-02-28 15:05:00 +0000

    feat(Misc): Basic type provider that works

--- src/misc/BasicProvider/BasicProvider.fs
+++ src/misc/BasicProvider/BasicProvider.fs
@@ -7,16 +7,14 @@ open System.Reflection
 
 [<TypeProvider>]
 type BasicProvider (config : TypeProviderConfig) as this =
-    inherit TypeProviderForNamespaces ()
+    inherit TypeProviderForNamespaces (config)
 
     let ns = "StaticProperty.Provided"
     let asm = Assembly.GetExecutingAssembly()
 
     let createTypes () =
         let myType = ProvidedTypeDefinition(asm, ns, "MyType", Some typeof<obj>)
-        let myProp = ProvidedProperty("MyProperty", typeof<string>) //, isStatic = true, getterCode = (fun args -> <@@ "Hello world" @@>))
-        myProp.IsStatic <- true
-        myProp.GetterCode <- fun args -> <@@ "Hello world" @@>
+        let myProp = ProvidedProperty("MyProperty", typeof<string>, isStatic = true, getterCode = (fun args -> <@@ "Hello world" @@>))
         myType.AddMember(myProp)
         [myType]
 
--- src/misc/BasicProvider/TestProvider.fsx
+++ src/misc/BasicProvider/TestProvider.fsx
@@ -1,1 +1,7 @@
+// Compile with: fsc -r:.\bin\Debug\BasicProvider.dll .\TestProvider.fsx
+
 #r @"bin/Debug/BasicProvider.dll"
+open StaticProperty.Provided
+
+type T = StaticProperty.Provided.MyType
+printfn "%s" T.MyProperty
