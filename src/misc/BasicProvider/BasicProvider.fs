81fe7773cbd4d14d424bb6f8840fc8351d4025e0
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-02-28 14:36:59 +0000

    feat(Misc): Simple type provider (not working)

---
+++ src/misc/BasicProvider/BasicProvider.fs
@@ -0,0 +1,27 @@
+﻿namespace BasicProvider
+
+open ProviderImplementation
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+open System.Reflection
+
+[<TypeProvider>]
+type BasicProvider (config : TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces ()
+
+    let ns = "StaticProperty.Provided"
+    let asm = Assembly.GetExecutingAssembly()
+
+    let createTypes () =
+        let myType = ProvidedTypeDefinition(asm, ns, "MyType", Some typeof<obj>)
+        let myProp = ProvidedProperty("MyProperty", typeof<string>) //, isStatic = true, getterCode = (fun args -> <@@ "Hello world" @@>))
+        myProp.IsStatic <- true
+        myProp.GetterCode <- fun args -> <@@ "Hello world" @@>
+        myType.AddMember(myProp)
+        [myType]
+
+    do
+        this.AddNamespace(ns, createTypes())
+
+[<assembly:TypeProviderAssembly>]
+do ()
---
+++ src/misc/BasicProvider/TestProvider.fsx
@@ -0,0 +1,1 @@
+#r @"bin/Debug/BasicProvider.dll"
