8af2e7454b1bc414b6700e1aa6fbe04229bbfc8d
Christos Gkantsidis <gkantsidis@gmail.com>
2018-02-28 02:09:07 +0000

    Revert "Revert "feat(Generator): Some more work on type provider, but still not compiling properly""
    This reverts commit 971593256872cfceb6df5a8d122e3fc41ad05caf.

--- src/YANG/Generator/YangProvider.fs
+++ src/YANG/Generator/YangProvider.fs
@@ -1,19 +1,108 @@
 ﻿namespace Yang.Generator
 
+// Some pointers to creating TypeProviders:
+// - <https://docs.microsoft.com/en-us/dotnet/fsharp/tutorials/type-providers/creating-a-type-provider>
+
 open System.IO
 open System.Reflection
 open Microsoft.FSharp.Core.CompilerServices
-open Microsoft.FSharp.Quotations
 open ProviderImplementation.ProvidedTypes
+open FParsec
+open Yang.Parser
 
 exception GenericYangProviderError of string
 
 [<TypeProvider>]
-type YangProvider (config: TypeProviderConfig) =
+type public YangProvider (config: TypeProviderConfig) as this =
     inherit TypeProviderForNamespaces()
 
-    let asm = (Assembly.LoadFrom(config.RuntimeAssembly))
+    /// Static parameters for type provider.
+    let staticParams =
+        [
+            // Specifies the file that contains the YANG model
+            ProvidedStaticParameter("file", typeof<string>)
+        ]
+
+    let asm = Assembly.LoadFrom(config.RuntimeAssembly)
     let schema = makeType asm "Yang"
+
+    /// Each provider needs a unique temporary file
     let provAsm = ProvidedAssembly(Path.ChangeExtension(Path.GetTempFileName(), ".dll"))
 
-    member __.X = "F#"
+    // Set the logger; observe that the logger will just write to console
+    do SetLogger !ProvidedTypeDefinition.Logger
+
+    /// Method for starting the type generation process
+    let schemaCreation =
+        fun (typeName:string) (parameterValues: obj[]) ->
+            match parameterValues with
+            | [| :? string as fileName |] ->
+                if File.Exists(fileName) = false then
+                    raise (GenericYangProviderError (sprintf "Cannot find model file '%s'" fileName))
+
+                let model = Parser.ReadAndClean fileName
+                if System.String.IsNullOrWhiteSpace(model) then
+                    raise (GenericYangProviderError "Input model cannot be empty")
+
+                match (run  Module.parse_module model) with
+                | Success (result, _, _)    ->
+                    typeName
+                    |> makeType asm
+                    |> addMembers (createTypes result)
+                    |> addIncludedType provAsm
+
+                | Failure (message, _, _)   -> raise (GenericYangProviderError (sprintf "Failed to parse model, error: %s" message))
+
+            | _ -> raise (GenericYangProviderError "Unexpected parameter values when using Yang provider")
+
+    do schema.DefineStaticParameters( parameters=staticParams, instantiationFunction=schemaCreation )
+    do this.AddNamespace(ns, [ addIncludedType provAsm schema ])
+
+/// This provider creates the type from a string;
+/// it is useful for testing, otherwise, it is very similar to the functionality above.
+[<TypeProvider>]
+type public YangFromStringProvider (config: TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces()
+
+    /// Static parameters for type provider.
+    let staticParams =
+        [
+            // This parameter is used to specify the model from which to generate types.
+            ProvidedStaticParameter("model", typeof<string>)
+        ]
+
+    let asm = Assembly.LoadFrom(config.RuntimeAssembly)
+    let schema = makeType asm "YangFromString"
+
+    /// Each provider needs a unique temporary file
+    let provAsm = ProvidedAssembly(Path.ChangeExtension(Path.GetTempFileName(), ".dll"))
+
+    // Set the logger; observe that the logger will just write to console
+    do SetLogger !ProvidedTypeDefinition.Logger
+
+    /// Method for starting the type generation process
+    let schemaCreation =
+        fun (typeName:string) (parameterValues: obj[]) ->
+            match parameterValues with
+            | [| :? string as model |] ->
+                let model' = Comments.Remove model
+                if System.String.IsNullOrWhiteSpace(model') then
+                    raise (GenericYangProviderError "Input model cannot be empty")
+
+                match (run  Module.parse_module model') with
+                | Success (result, _, _)    ->
+                    typeName
+                    |> makeType asm
+                    |> addMembers (createTypes result)
+                    |> addIncludedType provAsm
+
+                | Failure (message, _, _)   -> raise (GenericYangProviderError (sprintf "Failed to parse model, error: %s" message))
+
+            | _ -> raise (GenericYangProviderError "Unexpected parameter values when using Yang provider")
+
+    do schema.DefineStaticParameters( parameters=staticParams, instantiationFunction=schemaCreation )
+    do this.AddNamespace(ns, [ addIncludedType provAsm schema ])
+
+
+[<assembly:TypeProviderAssembly>]
+do ()
