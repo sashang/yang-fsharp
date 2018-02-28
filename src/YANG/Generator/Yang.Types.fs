971593256872cfceb6df5a8d122e3fc41ad05caf
Christos Gkantsidis <gkantsidis@gmail.com>
2018-02-28 01:52:50 +0000

    Revert "feat(Generator): Some more work on type provider, but still not compiling properly"
    This reverts commit 895bfbeea10eaba2387ad8896bbc3f0f6b5291ea.

--- src/YANG/Generator/Yang.Types.fs
+++ src/YANG/Generator/Yang.Types.fs
@@ -2,14 +2,7 @@
 
 [<AutoOpen>]
 module Types =
-    open System
-    open System.Reflection
     open ProviderImplementation.ProvidedTypes
-    open Yang.Parser
-
-    (*
-     * Boiler plate code for creating types
-     *)
 
     let private _logger : (string -> unit) option ref = ref None
 
@@ -21,51 +14,7 @@ module Types =
             | None -> ()
             | Some logger -> logger message
 
-    let ns = "Yang.Generator"
+    let ns = "Yang.YangProvider"
 
     let internal makeType asm typeName =
         ProvidedTypeDefinition( asm, ns, typeName, Some typeof<obj>, IsErased = false)
-
-    let internal addIncludedType (provAsm : ProvidedAssembly) (ty:ProvidedTypeDefinition) =
-        provAsm.AddTypes([ ty ])
-        ty
-
-    let internal addMembers (mi:#MemberInfo list) (ty:ProvidedTypeDefinition) =
-        ty.AddMembersDelayed (
-            fun() ->
-                // printfn "Delayed expansion for %s" ty.Name
-                mi
-        )
-        ty
-
-    let internal addMember (mi:#MemberInfo) (ty:ProvidedTypeDefinition) =
-        ty.AddMember mi
-        ty
-
-    (*
-     * end of boilerplate code
-     *)
-
-    let internal createTypes (_module : Module.Module) =
-        let (version, _) = _module.Header.YangVersion
-        let (ns, _) = _module.Header.Namespace
-        let (prefix, _) = _module.Header.Prefix
-
-        let header = [
-            ProvidedLiteralField ("YangVersion", typeof<Version>, version);
-            ProvidedLiteralField ("Namespace", typeof<Uri>, ns);
-            ProvidedLiteralField ("Prefix", typeof<string>, prefix)
-        ]
-
-        let meta =
-            match _module.Meta with
-            | None -> []
-            | Some meta ->
-                [
-                    meta.Contact        |> Option.map (fun (c, _) -> ProvidedLiteralField ("Contact", typeof<string>, c))
-                    meta.Description    |> Option.map (fun (d, _) -> ProvidedLiteralField ("Description", typeof<string>, d))
-                    meta.Organization   |> Option.map (fun (o, _) -> ProvidedLiteralField ("Organization", typeof<string>, o))
-                    meta.Reference      |> Option.map (fun (r, _) -> ProvidedLiteralField ("Reference", typeof<string>, r))
-                ] |> List.choose id
-
-        header @ meta
