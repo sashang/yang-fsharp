cf7d7670334802483685fdf7bc5439f135b55b18
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-03-01 13:09:17 +0000

    test(Generator): Initial project to test that the types generated can be consumed in C#.

---
+++ src/TypeTestsFromCSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TypeTestsFromCSharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TypeTestsFromCSharp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c8df4e2-1dd2-4c80-b24b-085a8ffdde0e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/TypeTestsFromCSharp/RFC7950-Example-System.cs
@@ -0,0 +1,41 @@
+﻿// ***********************************************************************
+// Assembly         : TypeTestsFromCSharp
+// Author           : chrisgk
+// Created          : 03-01-2018
+//
+// Last Modified By : chrisgk
+// Last Modified On : 03-01-2018
+// ***********************************************************************
+// <copyright file="RFC7950-Example-System.cs" company="">
+//     Copyright ©  2018
+// </copyright>
+// <summary>
+//  Tests that the types generated in F# can be consumed by C#.
+//  This file contains tests for the simple example from RFC 7950.
+// </summary>
+// ***********************************************************************
+
+namespace TypeTestsFromCSharp
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Yang.Examples.RFC7950.SimpleModel;
+
+    /// <summary>
+    /// Class Rfc7950ExampleSystem.
+    /// </summary>
+    [TestClass]
+    public class Rfc7950ExampleSystem
+    {
+        /// <summary>
+        /// Tests the reading header information.
+        /// </summary>
+        [TestMethod]
+        public void TestReadingHeaderInformation()
+        {
+            Assert.AreEqual("sys", Model.T.Information.Prefix);
+            Assert.AreEqual("Example Inc.", Model.T.Information.Organization);
+            Assert.AreEqual("joe@example.com", Model.T.Information.Contact);
+            Assert.AreEqual("The module for entities implementing the Example system.", Model.T.Information.Description);
+        }
+    }
+}
