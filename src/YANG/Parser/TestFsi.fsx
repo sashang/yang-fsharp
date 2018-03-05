c680e87ab5fc56bdc7fad1ee5d411efdef730a9d
Christos Gkantsidis <gkantsidis@gmail.com>
2018-03-05 20:49:59 +0000

    test(F# script to test that the interactive environment works):

---
+++ src/YANG/Parser/TestFsi.fsx
@@ -0,0 +1,10 @@
+﻿(*
+ * This script is just for testing the F# Interactive.
+ *)
+
+#r @"../Model/bin/Debug/Yang.Model.dll"
+
+open Yang.Model
+
+let body = LeafBodyStatement.Description ("Some test here", None)
+printfn "Description is: %A" body
