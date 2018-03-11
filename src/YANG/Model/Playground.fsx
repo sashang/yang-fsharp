8dd9117515243db28e2b781b26d7e1542bb811fb
Christos Gkantsidis <gkantsidis@gmail.com>
2018-03-11 23:55:35 +0000

    chore(Model): some comments to get started with creating types

--- src/YANG/Model/Playground.fsx
+++ src/YANG/Model/Playground.fsx
@@ -4,6 +4,7 @@
 #load @"../../../.paket/load/net471/NLog.fsx"
 
 #load "Errors.fs"
+#load "Generic.fs"
 #load "Identifier.fs"
 #load "Arguments.fs"
 #load "Expressions.fs"
@@ -12,6 +13,37 @@
 open System
 open Yang.Model
 
-let xx = YangVersion (Version(1,1), None)
+module ResolveType =
 
-Statement.ToString xx
+    open System.Collections.Specialized
+    open System
+
+    // [RFC 7950, p. 24]
+    // Name                 Description                         
+    //==========================================================
+    // binary               Any binary data                     
+    // bits                 A set of bits or flags              
+    // boolean              "true" or "false"                   
+    // decimal64            64=bit signed decimal number        
+    // empty                A leaf that does not have any value 
+    // enumeration          One of an enumerated set of strings 
+    // identityref          A reference to an abstract identity 
+    // instance=identifier  A reference to a data tree node     
+    // int8                 8=bit signed integer                
+    // int16                16=bit signed integer               
+    // int32                32=bit signed integer               
+    // int64                64=bit signed integer               
+    // leafref              A reference to a leaf instance      
+    // string               A character string                  
+    // uint8                8=bit unsigned integer              
+    // uint16               16=bit unsigned integer             
+    // uint32               32=bit unsigned integer             
+    // uint64               64=bit unsigned integer             
+    // union                Choice of member types              
+    //
+    // Example of 'bits' ([RFC 7950, p. 176]):
+    //type bits {
+    //    bit UP;
+    //    bit PROMISCUOUS
+    //    bit DISABLED;
+    //}
