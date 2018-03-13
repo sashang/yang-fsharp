5f2a63b1c4a2fe5af0a9ddc33a0fd02d91023886
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-03-13 10:10:01 +0000

    style(Model): Renaming of functions

--- src/YANG/Model/StatementHelper.fs
+++ src/YANG/Model/StatementHelper.fs
@@ -7,15 +7,15 @@ module StatementHelper =
     let private counter (update : 'T -> int) : 'T list -> int =
         List.fold (fun s v -> s + (update v)) 0
 
-    let private ocounter (update : 'T -> int) (input : 'T list option) =
+    let private counter_o (update : 'T -> int) (input : 'T list option) =
         if input.IsNone then 0
         else counter (fun v -> 1 + (update v)) (input.Value)
 
-    let private otcounter (transform : 'T -> Statement) (update : Statement -> int) (input : 'T list option) =
+    let private counter_ot (transform : 'T -> Statement) (update : Statement -> int) (input : 'T list option) =
         if input.IsNone then 0
         else counter (fun v -> 1 + (update (transform v))) (input.Value)
 
-    let private tcounter (transform : 'T -> Statement) (update : Statement -> int) (input : 'T list) =
+    let private counter_t (transform : 'T -> Statement) (update : Statement -> int) (input : 'T list) =
         counter (fun v -> 1 + (update (transform v))) input
 
     /// <summary>
@@ -53,80 +53,80 @@ module StatementHelper =
     | Statement.Value               (_, options)
     | Statement.YangVersion         (_, options)
     | Statement.YinElement          (_, options)
-        -> ocounter Descendants options
+        -> counter_o Descendants options
 
     // The following have custom options; the caller need to treat them specially
     | Statement.Action              (_, body) ->
-        otcounter ActionBodyStatement.Translate Descendants body
+        counter_ot ActionBodyStatement.Translate Descendants body
     | Statement.AnyData             (_, body) ->
-        otcounter AnyDataBodyStatement.Translate Descendants body
+        counter_ot AnyDataBodyStatement.Translate Descendants body
     | Statement.AnyXml              (_, body) ->
-        otcounter AnyXmlBodyStatement.Translate Descendants body
+        counter_ot AnyXmlBodyStatement.Translate Descendants body
     | Statement.Augment             (_, body) ->
-        tcounter AugmentBodyStatement.Translate Descendants body
+        counter_t AugmentBodyStatement.Translate Descendants body
     | Statement.Argument            (_, body) ->
-        otcounter ArgumentBodyStatement.Translate Descendants body
+        counter_ot ArgumentBodyStatement.Translate Descendants body
     | Statement.BelongsTo           (_, body) ->
-        tcounter BelongsToBodyStatement.Translate Descendants body
+        counter_t BelongsToBodyStatement.Translate Descendants body
     | Statement.Bit                 (_, body) ->
-        otcounter BitBodyStatement.Translate Descendants body
+        counter_ot BitBodyStatement.Translate Descendants body
     | Statement.Case                (_, body) ->
-        otcounter CaseBodyStatement.Translate Descendants body
+        counter_ot CaseBodyStatement.Translate Descendants body
     | Statement.Choice              (_, body) ->
-        otcounter ChoiceBodyStatement.Translate Descendants body
+        counter_ot ChoiceBodyStatement.Translate Descendants body
     | Statement.Container           (_, body) ->
-        otcounter ContainerBodyStatement.Translate Descendants body
+        counter_ot ContainerBodyStatement.Translate Descendants body
     | Statement.DeviateAdd          body ->
-        otcounter DeviateAddBodyStatement.Translate Descendants body
+        counter_ot DeviateAddBodyStatement.Translate Descendants body
     | Statement.DeviateDelete       body ->
-        otcounter DeviateDeleteBodyStatement.Translate Descendants body
+        counter_ot DeviateDeleteBodyStatement.Translate Descendants body
     | Statement.DeviateReplace      body ->
-        otcounter DeviateReplaceBodyStatement.Translate Descendants body
+        counter_ot DeviateReplaceBodyStatement.Translate Descendants body
     | Statement.Deviation           (_, body) ->
-        otcounter DeviationBodyStatement.Translate Descendants body
+        counter_ot DeviationBodyStatement.Translate Descendants body
     | Statement.Enum                (_, body) ->
-        otcounter EnumBodyStatement.Translate Descendants body
+        counter_ot EnumBodyStatement.Translate Descendants body
     | Statement.Extension           (_, body) ->
-        otcounter ExtensionBodyStatement.Translate Descendants body
+        counter_ot ExtensionBodyStatement.Translate Descendants body
     | Statement.Feature             (_, body) ->
-        otcounter FeatureBodyStatement.Translate Descendants body
+        counter_ot FeatureBodyStatement.Translate Descendants body
     | Statement.Grouping            (_, body) ->
-        otcounter GroupingBodyStatement.Translate Descendants body
+        counter_ot GroupingBodyStatement.Translate Descendants body
     | Statement.Identity            (_, body) ->
-        otcounter IdentityBodyStatement.Translate Descendants body
+        counter_ot IdentityBodyStatement.Translate Descendants body
     | Statement.Import              (_, body) ->
-        tcounter ImportBodyStatement.Translate Descendants body
+        counter_t ImportBodyStatement.Translate Descendants body
     | Statement.Include             (_, body) ->
-        otcounter IncludeBodyStatement.Translate Descendants body
+        counter_ot IncludeBodyStatement.Translate Descendants body
     | Statement.Input               body ->
-        tcounter InputBodyStatement.Translate Descendants body
+        counter_t InputBodyStatement.Translate Descendants body
     | Statement.Leaf                (_, body) ->
-        tcounter LeafBodyStatement.Translate Descendants body
+        counter_t LeafBodyStatement.Translate Descendants body
     | Statement.LeafList            (_, body) ->
-        tcounter LeafListBodyStatement.Translate Descendants body
+        counter_t LeafListBodyStatement.Translate Descendants body
     | Statement.Length              (_, body) ->
-        otcounter LengthBodyStatement.Translate Descendants body
+        counter_ot LengthBodyStatement.Translate Descendants body
     | Statement.List                (_, body) ->
-        tcounter ListBodyStatement.Translate Descendants body
+        counter_t ListBodyStatement.Translate Descendants body
     | Statement.Module              body ->
         // TODO: counter number of statements in module
         10
     | Statement.Must                (_, body) ->
-        otcounter MustBodyStatement.Translate Descendants body
+        counter_ot MustBodyStatement.Translate Descendants body
     | Statement.Notification        (_, body) ->
-        otcounter NotificationBodyStatement.Translate Descendants body
+        counter_ot NotificationBodyStatement.Translate Descendants body
     | Statement.Output              body ->
-        tcounter OutputBodyStatement.Translate Descendants body
+        counter_t OutputBodyStatement.Translate Descendants body
     | Statement.Pattern             (_, body) ->
-        otcounter PatternBodyStatement.Translate Descendants body
+        counter_ot PatternBodyStatement.Translate Descendants body
     | Statement.Range               (_, body) ->
-        otcounter RangeBodyStatement.Translate Descendants body
+        counter_ot RangeBodyStatement.Translate Descendants body
     | Statement.Rpc                 (_, body) ->
-        otcounter RpcBodyStatement.Translate Descendants body
+        counter_ot RpcBodyStatement.Translate Descendants body
     | Statement.Refine              (_, body) ->
-        otcounter RefineBodyStatement.Translate Descendants body
+        counter_ot RefineBodyStatement.Translate Descendants body
     | Statement.Revision            (_, body) ->
-        otcounter RevisionBodyStatement.Translate Descendants body
+        counter_ot RevisionBodyStatement.Translate Descendants body
     | Statement.Submodule           body ->
         // TODO: counter number of statements in sub-module
         10
@@ -141,13 +141,13 @@ module StatementHelper =
         let r = 0
         e + r
     | Statement.TypeDef             (_, body) ->
-        tcounter TypeDefBodyStatement.Translate Descendants body
+        counter_t TypeDefBodyStatement.Translate Descendants body
     | Statement.Uses                (_, body) ->
-        otcounter UsesBodyStatement.Translate Descendants body
+        counter_ot UsesBodyStatement.Translate Descendants body
     | Statement.UsesAugment         (_, body) ->
-        tcounter UsesAugmentBodyStatement.Translate Descendants body
+        counter_t UsesAugmentBodyStatement.Translate Descendants body
     | Statement.When                (_, body) ->
-        otcounter WhenBodyStatement.Translate Descendants body
+        counter_ot WhenBodyStatement.Translate Descendants body
 
     | Statement.Unknown body ->
         let _, _, _extra = body
