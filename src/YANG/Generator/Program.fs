dfe15750e956d5c203faf657e3a9b243ea90bf36
Christos Gkantsidis <gkantsidis@gmail.com>
2018-03-19 08:04:02 +0000

    feat(Generator-C#): Early work on intermediate language before generating program

---
+++ src/YANG/Generator/Program.fs
@@ -0,0 +1,45 @@
+﻿// Program.fs
+// Intermediate representation for the library that will be generated
+namespace Yang.Generator
+
+module Program =
+    open Yang.Model
+
+    type UInt32Type = {
+        Description     : string option
+        Range           : Arguments.Range option
+        Default         : System.UInt32 option
+    }
+
+    type StringType = {
+        Description     : string option
+        Restrictions    : Statements.StringRestrictions
+        Default         : string option
+    }
+    with
+        member this.HasRestrictions =
+            match this.Restrictions with
+            | None, []  -> false
+            | _         -> true
+
+    type Type =
+    | String of StringType
+    | Class of string
+
+    type YangInfo = | YangInfo of Name:string * Description:(string option)
+
+    type Definition =
+    | Class         of ClassDefinition
+    | Member        of MemberDefinition * Type
+    | ListWithKey   of Member:MemberDefinition * Type:Type * Key:MemberDefinition
+    and ClassDefinition = {
+        Info            : YangInfo
+        Name            : string option
+        Body            : Definition list
+    }
+    and MemberDefinition = {
+        Info            : YangInfo
+        Name            : string option
+        Optional        : bool
+    }
+
