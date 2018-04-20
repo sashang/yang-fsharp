83d08f0c6e32783c61b0cc9c8e57e3bcf13775a4
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-04-20 09:46:30 +0100

    feat(Project): Added reference to OpenConfig repo for yang models

--- Models-External/Download.ps1
+++ Models-External/Download.ps1
@@ -4,12 +4,13 @@ param(
 )
 
 [PSCustomObject[]]$repositories = @(
-    [PSCustomObject]@{ Name = "BroadbandForum";   Link = "https://github.com/BroadbandForum/yang.git" },
-    [PSCustomObject]@{ Name = "Juniper";   Link = "https://github.com/Juniper/yang.git" },
-    [PSCustomObject]@{ Name = "networkop";   Link = "https://github.com/networkop/yang.git" },
-    [PSCustomObject]@{ Name = "sysrepo";   Link = "https://github.com/sysrepo/sysrepo.git" },
-    [PSCustomObject]@{ Name = "tail-f-systems";   Link = "https://github.com/tail-f-systems/JNC.git" },
-    [PSCustomObject]@{ Name = "YangModels";   Link = "https://github.com/YangModels/yang.git" }
+    [PSCustomObject]@{ Name = "BroadbandForum"; Link = "https://github.com/BroadbandForum/yang.git" },
+    [PSCustomObject]@{ Name = "Juniper";        Link = "https://github.com/Juniper/yang.git" },
+    [PSCustomObject]@{ Name = "networkop";      Link = "https://github.com/networkop/yang.git" },
+    [PSCustomObject]@{ Name = "openconfig";     Link = "https://github.com/openconfig/public.git" },
+    [PSCustomObject]@{ Name = "sysrepo";        Link = "https://github.com/sysrepo/sysrepo.git" },
+    [PSCustomObject]@{ Name = "tail-f-systems"; Link = "https://github.com/tail-f-systems/JNC.git" },
+    [PSCustomObject]@{ Name = "YangModels";     Link = "https://github.com/YangModels/yang.git" }
 )
 
 Push-Location -Path $PSScriptRoot
