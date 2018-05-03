279739abb2f45824a57a0f668d8d327e2651e2eb
Christos Gkantsidis <Christos.Gkantsidis@microsoft.com>
2018-05-03 20:09:37 +0100

    feat(Misc): Added another source of YANG models

--- Models-External/Download.ps1
+++ Models-External/Download.ps1
@@ -8,6 +8,7 @@ param(
     [PSCustomObject]@{ Name = "Juniper";        Link = "https://github.com/Juniper/yang.git" },
     [PSCustomObject]@{ Name = "networkop";      Link = "https://github.com/networkop/yang.git" },
     [PSCustomObject]@{ Name = "openconfig";     Link = "https://github.com/openconfig/public.git" },
+    [PSCustomObject]@{ Name = "sonic-object-model-schema"; Link = "https://github.com/Azure/sonic-object-model-schema.git"},
     [PSCustomObject]@{ Name = "sysrepo";        Link = "https://github.com/sysrepo/sysrepo.git" },
     [PSCustomObject]@{ Name = "tail-f-systems"; Link = "https://github.com/tail-f-systems/JNC.git" },
     [PSCustomObject]@{ Name = "YangModels";     Link = "https://github.com/YangModels/yang.git" }
